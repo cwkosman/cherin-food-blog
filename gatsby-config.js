3d7e1c1db5ef02750214ef78f95fee6258267efd
Chuck Kosman <cwkosman@gmail.com>
2020-05-18 10:54:59 -0700

    Humble beginnings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,10 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    title: `Title TBD`,
+    author: `Erin Quon & Chuck Kosman`,
+    description: `A no nonsense food blog`,
+    siteUrl: `https://cherin-food-blog.netlify.com/`,
   },
   plugins: [
     `gatsby-plugin-netlify-cms`,
--- src/components/seo.js
+++ src/components/seo.js
@@ -36,6 +36,10 @@ function SEO({ description, lang, meta, keywords, title }) {
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
+          name: `robots`,
+          content: `noindex`,
+        },
+        {
           name: `description`,
           content: metaDescription,
         },
